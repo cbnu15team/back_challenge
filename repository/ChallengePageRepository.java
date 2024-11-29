382d7dd9ef4c066f3d4e11ab59f079d0b5eb4f5a
coalsld <152403126+coalsld@users.noreply.github.com>
2024-11-29 11:18:57 +0900

    Delete repository directory

--- repository/ChallengePageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.joinup.challengeboard.repository;
-
-import com.example.joinup.challengeboard.entity.ChallengePage;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChallengePageRepository extends JpaRepository<ChallengePage, Integer> {
-}
