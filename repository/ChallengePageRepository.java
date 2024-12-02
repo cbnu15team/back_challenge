7cc34aca709aadeeb72fcbff5fa34783ee62ae25
coalsld <152403126+coalsld@users.noreply.github.com>
2024-12-02 21:12:02 +0900

    Update ChallengePageRepository.java

--- repository/ChallengePageRepository.java
+++ repository/ChallengePageRepository.java
@@ -2,8 +2,6 @@ package com.example.joinup.challengeboard.repository;
 
 import com.example.joinup.challengeboard.entity.ChallengePage;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface ChallengePageRepository extends JpaRepository<ChallengePage, Integer> {
+public interface ChallengePageRepository extends JpaRepository<ChallengePage, Long> {
 }
